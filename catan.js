aaf4d34a46d31c1d21635279772a9d1e870b1969
danielj0nes <daniel@jones.ac>
2022-08-07 03:37:31 +0200

    Now fully tracking dice roll statistics
    To do: d3.js to make it into a nice chart

--- catan.js
+++ catan.js
@@ -1,6 +1,8 @@
 const playerLogsId = 'logs';
 const playerNamesId = 'player-name'
+const rollIndicator = 'rolls dice:'
 let previousLog = 0;
+let diceStats = {2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0};
 console.log('Extension loaded :)');
 browser.storage.local.set({ game: 'Catan' });
 
@@ -14,19 +16,40 @@ if (playerElements) {
     browser.storage.local.set({ players: playerNames});
 }
 
-// Parse the logs
-function logParser(log) {
-    let logNumber = log[0].id.split('_')[1];
+// Parse the dice rolls, helper function for logScraper
+function parseDice(log) {
+    if (log.includes(rollIndicator)) {
+        const matchDice = /icon_die([0-9]) .* icon_die([0-9])/;
+        let [_, d1, d2] = log.match(matchDice)
+        d1 = parseInt(d1); d2 = parseInt(d2);
+        const roll = d1 + d2;
+        diceStats[roll] += 1;
+        console.log(diceStats);
+        browser.storage.local.set({ dice: diceStats });
+    }   
+}
+
+
+// Scrape the logs from the game and obtain the inner HTML for later parsing
+function logScraper(log) {
+    let logNumber = parseInt(log[0].id.split('_')[1]);
+    // Handle case where multiple logs are fired at once
+    // Here we want to obtain and parse all the logs the observer didn't get (previousLog + 1 : logNumber - 1)
     if (logNumber - 1 > previousLog && previousLog !== 0) {
-        console.log(previousLog, logNumber);
+        let logRange = (logNumber - previousLog) - 1;
+        let toNumber = previousLog + 1;
+        // Use logRange to work out how many iterations and toNumber as the stopping point
+        for (let logId of [...Array(logRange).keys()].map(i => i + toNumber)) {
+            let missingLog = document.getElementById(`log_${logId}`);
+            parseDice(missingLog.innerHTML);
+            console.log(missingLog.innerHTML);
+        }
     }
-    console.log(log);
+    // Save the most recent log into memory (will probs remove this)
     console.log(log[0].innerHTML);
-
-    // Get div_id
-    
-
+    parseDice(log[0].innerHTML);
     browser.storage.local.set({ BGA: log[0].innerHTML });
+    // Update the last log number, for multiple logs case
     previousLog = logNumber;
     
 }
@@ -36,7 +59,7 @@ let gameLog = new MutationObserver(function (e) {
     if (e[0].addedNodes) {
         let log = e[0].addedNodes;
 
-        logParser(log);
+        logScraper(log);
     }
 });
 gameLog.observe(document.getElementById(playerLogsId), { childList: true });
--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 // Update the popout menu of the extension
 
 browser.storage.local.get('players').then(result => {
-    for (let name of result.players) {
-        let playerNameElement = document.createElement('div');
-        playerNameElement.className = 'player-name'
-        playerNameElement.id = name;
-        playerNameElement.innerText = name;
-        document.getElementById('players').appendChild(playerNameElement)
+    if (result.players) {
+        for (let name of result.players) {
+            let playerNameElement = document.createElement('div');
+            playerNameElement.className = 'player-name'
+            playerNameElement.id = name;
+            playerNameElement.innerText = name;
+            document.getElementById('players').appendChild(playerNameElement)
+        }
     }
 });
 
@@ -14,7 +16,7 @@ browser.storage.local.get('game').then(result => {
     if (result.game) {
         document.getElementById('game').innerText = result.game;
     } else {
-        document.getElementById('game').innerText = "No games currently in progress";
+        document.getElementById('game').innerText = 'No games currently in progress';
     }
 });
 
@@ -22,12 +24,22 @@ browser.storage.local.get('BGA').then(result => {
     if (result.BGA) {
         document.getElementById('game-stats').innerText = result.BGA;
     } else {
-        document.getElementById('game-stats').innerText = "";
+        document.getElementById('game-stats').innerText = '';
     }
 });
 
+browser.storage.local.get('dice').then(result => {
+    if (result.dice) {
+        document.getElementById('dice-rolls').style.display = "";
+        for (const [diceNum, diceAmount] of Object.entries(result.dice)) {
+            document.getElementById(`dice-${diceNum}-amount`).innerText = diceAmount;
+        }
+    }
+});
+
+// Dynamically update values
 browser.storage.onChanged.addListener( () => {
     browser.storage.local.get('BGA').then(result => {
         document.getElementById('game-stats').innerText = result.BGA;
-    })
+    });
   });
